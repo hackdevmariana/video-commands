aed2663429782d496f00c2663f491a33014d1714
fernando <fernando@juandemariana.org>
2024-03-20 00:42:13 +0100

    Add smooth subcommand in appyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -188,6 +188,23 @@ def doodlepen(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def smooth(input, output):
+    """Applies smooth effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_smooth.png"
+
+    gmic.run(f'{input} smooth 80,0,1,1,2 output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 if __name__ == '__main__':
