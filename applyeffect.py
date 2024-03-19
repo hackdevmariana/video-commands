c937b0220074122abb7d4d1d61ee566de7a65911
fernando <fernando@juandemariana.org>
2024-03-20 00:22:46 +0100

    Add posterize subcommand in appyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -153,6 +153,23 @@ def cartoon(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def posterize(input, output):
+    """Applies posterize effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_posterize.png"
+
+    gmic.run(f'{input} fx_posterize 8,8,1,5,0,0,1,0 output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 if __name__ == '__main__':
