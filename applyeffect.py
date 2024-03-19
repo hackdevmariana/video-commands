0dd62efe358c3a441e20c3ee79ca39c396e86358
fernando <fernando@juandemariana.org>
2024-03-20 00:34:12 +0100

    Add doodlepen subcommand in appyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -171,6 +171,23 @@ def posterize(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def doodlepen(input, output):
+    """Applies doodle pen effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_doodle_pen.png"
+
+    gmic.run(f'{input} fx_feltpen 500,50,5,0.1,5,1,0 output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 if __name__ == '__main__':
