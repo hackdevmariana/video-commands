2b32b0176c063bed7f6dbad0b0ec0a339ce79a48
fernando <fernando@juandemariana.org>
2024-03-19 23:55:54 +0100

    Add judgment draw subcommand in appyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -84,5 +84,24 @@ def newspaperdotted(input, output):
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def judgment(input, output):
+    """Applies judgment draw effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_judgment_draw.png"
+
+    gmic.run(f'{input} +sketchbw 20 reverse blur[-1] 3 blend[-2,-1] overlay gui_merge_layers output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
 if __name__ == '__main__':
     cli()
