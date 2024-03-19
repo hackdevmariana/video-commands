d09e58167fab0aa7ee0bf1f7ee793fa4b115aa9a
fernando <fernando@juandemariana.org>
2024-03-20 00:07:02 +0100

    Add oilpainting subcommand in appyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -119,6 +119,23 @@ def polaroid(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def oilpainting(input, output):
+    """Applies oil painting effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_oil_painting.png"
+
+    gmic.run(f'{input} fx_vector_painting 9.5,0 output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 if __name__ == '__main__':
