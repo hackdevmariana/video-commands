e458bbe2f75e22a0ee4b40b21f209807b298a08b
fernando <fernando@juandemariana.org>
2024-03-20 00:02:55 +0100

    Add polaroid subcommand in appyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -102,6 +102,24 @@ def judgment(input, output):
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def polaroid(input, output):
+    """Applies polaroid effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_poster_hope.png"
+
+    gmic.run(f'{input} to_rgba polaroid 5,30 output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
 
 if __name__ == '__main__':
     cli()
