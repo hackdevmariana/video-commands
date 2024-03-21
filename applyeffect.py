8c1371c4e0f7ea96dadbb2ffeac6d5df7f984b98
fernando <fernando@juandemariana.org>
2024-03-21 21:07:25 +0100

    Add blury effect in applyeffect.py

--- applyeffect.py
+++ applyeffect.py
@@ -54,6 +54,24 @@ def blurx(input, output, intensity):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+@click.option('--intensity', type=int, default=20, help='Blur intensity')
+def blury(input, output, intensity):
+    """Applies blur effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_blurred_y.png"
+
+    gmic.run(f'{input} blur_y {intensity} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 @cli.command()
