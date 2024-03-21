1137d530224059ee557e0c4a523fe1c55683a8d1
fernando <fernando@juandemariana.org>
2024-03-21 23:35:10 +0100

    Add difference of gaussian effect in applyeffect.py

--- applyeffect.py
+++ applyeffect.py
@@ -453,6 +453,24 @@ def luminance(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def gaussiandog(input, output):
+    """Applies difference of gaussian effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_gaussiandog.png"
+
+    instruction = "dog 1,7"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 if __name__ == '__main__':
