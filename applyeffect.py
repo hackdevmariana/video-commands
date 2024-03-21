952de18aca9f929fc97121f496056817912e4ec8
fernando <fernando@juandemariana.org>
2024-03-21 23:40:45 +0100

    Add erode effect in applyeffect.py

--- applyeffect.py
+++ applyeffect.py
@@ -471,6 +471,24 @@ def gaussiandog(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+@click.option('--intensity', type=int, default=5, help='Blur intensity')
+def erode(input, output):
+    """Applies erode effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_erode.png"
+
+    instruction = "erode 5"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
 if __name__ == '__main__':
