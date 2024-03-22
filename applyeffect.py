85b3b240d0c68289b57630b236f42ad255cf7ddb
fernando <fernando@juandemariana.org>
2024-03-22 11:34:45 +0100

    Add removepixels effect in applyeffect.py

--- applyeffect.py
+++ applyeffect.py
@@ -565,6 +565,26 @@ def oldgame(input, output):
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+@click.option('--intensity', type=int, default=50, help='Blur intensity')
+def removepixels(input, output, intensity):
+    """Applies 8 bits effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_remove_pixels.png"
+
+    instruction = "remove_pixels"
+    gmic.run(f'{input} {instruction} {intensity}% output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
 
 if __name__ == '__main__':
     cli()
