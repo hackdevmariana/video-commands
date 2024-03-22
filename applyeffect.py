a3f31624af0513abaa3a4805cb94147588a3f861
fernando <fernando@juandemariana.org>
2024-03-23 00:43:51 +0100

    Add mosaic effect in applyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -785,6 +785,25 @@ def halftone(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def mosaic(input, output):
+    """Applies polygonize effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_mosaic.png"
+
+    instruction = 'mosaic , +fill "I!=J(1) || I!=J(0,1)?[0,0,0]:I" gui_merge_layers'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
 
 if __name__ == '__main__':
     cli()
