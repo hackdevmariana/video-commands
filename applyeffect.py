2f8c6fdd777515ff40f6353e639dcd9f7fa53783
fernando <fernando@juandemariana.org>
2024-03-22 12:54:32 +0100

    Add drawwhirl effect in applyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -711,6 +711,24 @@ def boxfitting(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def drawwhirl(input, output):
+    """Applies polygonize effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_draw_whirl.png"
+
+    instruction = 'draw_whirl ,'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 if __name__ == '__main__':
