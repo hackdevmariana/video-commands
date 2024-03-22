0a32699870b98e4f28a14f0ea9a52a0551c6c963
fernando <fernando@juandemariana.org>
2024-03-22 12:31:22 +0100

    Add sponged effect in applyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -620,6 +620,23 @@ def emanation(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def sponged(input, output):
+    """Applies sponged effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_sponged.png"
+
+    instruction = 'fx_lylejk_painting 10,2,4,10,0'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
 if __name__ == '__main__':
