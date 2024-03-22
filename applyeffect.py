6134689954c3a7b46c7daa1f0b04fd0fc216686b
fernando <fernando@juandemariana.org>
2024-03-22 12:44:09 +0100

    Add polygonize effect in applyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -656,6 +656,24 @@ def markerpen(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def polygonize(input, output):
+    """Applies polygonize effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_polygonize.png"
+
+    instruction = 'blur 1 fx_polygonize_delaunay 40,5,75,0.5,3,50,0,0,0,255,1,0,50,50'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 
