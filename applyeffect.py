0ca943990cfa85669678cc34b3c39987ef135d40
fernando <fernando@juandemariana.org>
2024-03-22 12:48:37 +0100

    Add pendrawing effect in applyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -675,6 +675,24 @@ def polygonize(input, output):
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def pendrawing(input, output):
+    """Applies polygonize effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_pendrawing.png"
+
+    instruction = 'fx_sketchbw 3,45,180,30,1.75,0.02,0.5,0.75,0.1,0.7,3,6,0,1,4,0,0,50,50'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 if __name__ == '__main__':
