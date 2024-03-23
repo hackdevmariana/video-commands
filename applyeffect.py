1d025c3b0a304e3dd6215e5b5ce1f05a5832b304
fernando <fernando@juandemariana.org>
2024-03-23 01:03:30 +0100

    Add grid effect in applyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -842,6 +842,65 @@ def puzzle(input, output):
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def arrayfade(input, output):
+    """Applies array fade effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_array_fade.png"
+
+    instruction = 'array_fade 2,2'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def framepainting(input, output):
+    """Frame painting to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_frame_painting.png"
+
+    instruction = 'frame_painting ,'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+@click.option('--intensity', type=int, default=20, help='Blur intensity')
+def grid(input, output, intensity):
+    """Convert the received image in grid."""
+
+    if not output:
+        output = f"{Path(input).stem}_grid_{intensity}.png"
+
+    instruction = 'imagegrid'
+    gmic.run(f'{input} {instruction} {intensity} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
 
 if __name__ == '__main__':
     cli()
