3167f805b3dce8629234705f5fa1972f2ab8d4ae
fernando <fernando@juandemariana.org>
2024-03-23 01:09:00 +0100

    Add tunnel effect in applyeffect command

--- applyeffect.py
+++ applyeffect.py
@@ -861,6 +861,46 @@ def arrayfade(input, output):
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+@click.option('--intensity', type=int, default=50, help='Blur intensity')
+def tiles(input, output, intensity):
+    """Applies tiles effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_tiles.png"
+
+    instruction = f'to_rgba rotate_tiles 10,{intensity} drop_shadow 10,10'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+@click.option('--intensity', type=int, default=50, help='Blur intensity')
+def tunnel(input, output, intensity):
+    """Applies tiles effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_tunnel.png"
+
+    instruction = f'tunnel {intensity}'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
