a3ff6bb1d78d719c28e2522242dbc0219e43929e
fernando <fernando@juandemariana.org>
2024-03-27 21:39:32 +0100

    Add poster hope in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -1257,6 +1257,26 @@ def fxglow(input, output):
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def prueba(input, output):
+    """Applies lomo to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_lomo.png"
+
+    instruction = 'fx_poster_hope 0,3,0'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
