b811a09d78b1c551b2e66173ae71a6138f8cbb7e
fernando <fernando@juandemariana.org>
2024-03-31 04:44:55 +0200

    Add solarize in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -563,6 +563,24 @@ def mineralmosaic(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def solarize(input, output):
+    """Applies solarize effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_solarize.png"
+
+    instruction = "solarize"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def dices(input, output):
     """Applies black and white dice texture to the received image."""
 
