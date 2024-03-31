cd39c1b4a1601a16d5162ce3263aac4697276b7c
fernando <fernando@juandemariana.org>
2024-03-31 04:26:47 +0200

    Add maze in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -509,6 +509,24 @@ def alienvision(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def maze(input, output):
+    """Create an image with maze to size of the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_alien_vision.png"
+
+    instruction = "fx_maze 24,1,0,1,0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def telesketch(input, output):
     """Applies telesketch effect to the received image."""
 
