4e903699cef6142d62a14bef2156ee0ee97258a6
fernando <fernando@juandemariana.org>
2024-04-01 10:24:44 +0200

    Add lightrays in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -721,6 +721,25 @@ def imagetunnel(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def lightrays(input, output):
+    """Applies lightrays in black and white to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_lightrays.png"
+
+    instruction = "fx_lightrays 80,50,50,1,0.5,0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def lightpatch(input, output):
     """Applies a light patch effect to the received image."""
 
