fe1ede0a325e118eab707247411dd58c9b1fee30
fernando <fernando@juandemariana.org>
2024-04-01 09:57:42 +0200

    Add burnoldphoto in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -32,6 +32,7 @@ effects = [
             'fractalize',
             'waxpaint',
             'feltpen',
+            'burnoldphoto',
             'brokenglass',
             'dices',
             'intensifies',
@@ -649,6 +650,24 @@ def framefuzzy(input, output):
         output = f"{Path(input).stem}_frame_fuzzy.png"
 
     instruction = "fx_frame_fuzzy 5,5,10,1,255,255,255,255"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def burnoldphoto(input, output):
+    """Applies a burny old photo effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_burn_old_photo.png"
+
+    instruction = "fx_burn 0.5,30,1,0,0,0"
     gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
