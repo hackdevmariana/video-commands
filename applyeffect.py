47d0c194d454effb648cdba072edb99df4d4f675
fernando <fernando@juandemariana.org>
2024-04-01 10:07:42 +0200

    Add imagetunnel in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -26,6 +26,7 @@ effects = [
             'gridtriangular',
             'warhol',
             'oilbw',
+            'vignette',
             'tiles',
             'framefuzzy',
             'rotatetiles',
@@ -67,6 +68,7 @@ effects = [
             'kuwahara',
             'normalizelocal',
             'oldgame',
+            'imagetunnel',
             'removepixels',
             'glitteronblack',
             'emanation',
@@ -657,11 +659,12 @@ def framefuzzy(input, output):
         click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
-def prueba(input, output):
-    """Applies a frame fuzzy to the received image."""
+def vignette(input, output):
+    """Applies a vignette effect to the received image."""
 
     if not output:
         output = f"{Path(input).stem}_vignette.png"
@@ -674,6 +677,25 @@ def prueba(input, output):
         click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def imagetunnel(input, output):
+    """Applies a all image tunnel effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_image_tunnel.png"
+
+    instruction = "fx_tunnel 4,80,0.5,0.5,0.2,0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
