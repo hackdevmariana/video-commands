34b73ecbc0940a1816a9eebd99e603f077d81f0f
fernando <fernando@juandemariana.org>
2024-04-01 11:12:03 +0200

    Add sectionalboxfitting in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -2096,6 +2096,26 @@ def lightrelief(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def sectionalboxfitting(input, output):
+    """Applies sectional boxfitting to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_sectional_boxfitting.png"
+
+    instruction = 'fx_boxfitting 3,0,0.1,0'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def shadowpatch(input, output):
     """Applies shadow patch to the received image."""
 
