a5abd055f018ee901df13775d6d063ab0ddbfd55
fernando <fernando@juandemariana.org>
2024-04-02 03:40:12 +0200

    Add lava effect in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -62,6 +62,7 @@ effects = [
             'cracks',
             'scanlines',
             'badprinter',
+            'lava',
             'telesketch',
             'badtonner',
             'deriche',
@@ -2092,6 +2093,27 @@ def lightrelief(input, output):
         click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def lava(input, output):
+    """Applie lava effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_lava.png"
+
+    instruction = 'fx_lava 8,5,3,0,0'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 
