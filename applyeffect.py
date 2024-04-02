0d8f16ef8b78b4a12c285860a9eced9d83fb5594
fernando <fernando@juandemariana.org>
2024-04-02 03:55:40 +0200

    Add whirls texture in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -2201,6 +2201,25 @@ def paper(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def whirls(input, output):
+    """Applie whirls texture to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_whirls.png"
+
+    instruction = 'fx_whirls 7,2,0.2,1.8,11,0'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def sackcloth(input, output):
     """Applie sackcloth texture to the received image."""
 
