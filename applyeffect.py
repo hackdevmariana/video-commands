6b0645bf4da3c5aa513e76434a6ef8f2e9950b8a
fernando <fernando@juandemariana.org>
2024-04-05 01:00:42 +0200

    Add edge offsets effect in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -34,6 +34,7 @@ effects = [
             'frameround',
             'crystal',
             'charcoal',
+            'edgeoffsets',
             'gradientnorm',
             'gradientrgb',
             'isophotes',
@@ -450,6 +451,25 @@ def charcoal(input, output):
         click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def edgeoffsets(input, output):
+    """Applies edge offsets effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_edge_offsets.png"
+
+    gmic.run(f'{input} fx_edge_offsets 0,15,4,1,0,0 output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 
