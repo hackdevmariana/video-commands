107bc463cee4d000506056fbb11e3c5da5bcc1a4
fernando <fernando@juandemariana.org>
2024-04-05 00:53:13 +0200

    Add gradientrgb effect in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -34,6 +34,7 @@ effects = [
             'frameround',
             'crystal',
             'charcoal',
+            'gradientrgb',
             'isophotes',
             'embossedmetal',
             'waxstains',
@@ -601,6 +602,25 @@ def isophotes(input, output):
         output = f"{Path(input).stem}_embossed_metal.png"
 
     instruction = "fx_isophotes 8,0,1,0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def gradientrgb(input, output):
+    """Applies gradient rgb effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_gradient_rgb.png"
+
+    instruction = "fx_gradient2rgb 0,0,100,0,0,0"
     gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
