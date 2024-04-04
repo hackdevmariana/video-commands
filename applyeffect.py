bca96f1a7a042ddade052b151a571f1bbc3e63fe
fernando <fernando@juandemariana.org>
2024-04-04 18:39:22 +0200

    Add wax stains effect in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -34,6 +34,7 @@ effects = [
             'frameround',
             'crystal',
             'charcoal',
+            'waxstains',
             'sectionalboxfitting',
             'framefuzzy',
             'rotatetiles',
@@ -542,6 +543,24 @@ def badprinter(input, output):
     imagen_or = ImageChops.logical_or(imagen1, imagen2)
     imagen_or.save(output)
     os.remove(temp_file)
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def waxstains(input, output):
+    """Applies wax stains effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_wax_stains.png"
+
+    instruction = "fx_segment_watershed 2,1,0,0,0"
+    gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
     if output_path.is_file():
