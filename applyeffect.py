895c5d14c42c782565d3baf81d9d161da0451460
fernando <fernando@juandemariana.org>
2024-04-11 01:48:57 +0200

    Add gaussian blur effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -21,6 +21,7 @@ effects = [
             'blurx',
             'blury',
             'blurradial',
+            'gaussianblur',
             'oldphoto',
             'newspaperdotted',
             'judgment',
@@ -625,6 +626,28 @@ def twirl(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+@click.option('--intensity', type=int, default=5, help='Blur intensity')
+def gaussianblur(input, output, intensity):
+    """Applies gaussian blur effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_gaussianblur.png"
+
+    instruction = f"fx_gaussian_blur {intensity},0,0,1,0,0,0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def blurradial(input, output):
     """Applies blur radial effect to the received image."""
 
@@ -639,8 +662,6 @@ def blurradial(input, output):
         click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
-
-
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
