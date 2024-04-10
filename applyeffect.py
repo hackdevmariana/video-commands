ee6dfeaf1ec39510dc7e106309afc6411b4b9416
fernando <fernando@juandemariana.org>
2024-04-11 01:55:51 +0200

    Add wind effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -648,6 +648,26 @@ def gaussianblur(input, output, intensity):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def wind(input, output):
+    """Applies wind effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_wind.png"
+
+    instruction = "fx_wind 20,0,0.7,20,1,0,0,0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def blurangular(input, output):
     """Applies blur angular effect to the received image."""
 
