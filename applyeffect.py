ae9a3804ab7578c4f4ada194612bd75c26aa7a11
fernando <fernando@juandemariana.org>
2024-04-10 20:17:48 +0200

    Add noise effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -28,6 +28,7 @@ effects = [
             'lightpatch',
             'twirl',
             'oilbw',
+            'noise',
             'reflect',
             'hearts',
             'vignette',
@@ -618,6 +619,25 @@ def twirl(input, output):
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+@click.option('--intensity', type=int, default=10, help='Waves intensity')
+def noise(input, output, intensity):
+    """Applies noise effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_noise.png"
+
+    instruction = f"fx_noise {intensity},0,0,1,0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
