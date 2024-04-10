0325932c50a0b5f1c7d9c8d24bf6470141966144
fernando <fernando@juandemariana.org>
2024-04-11 01:44:20 +0200

    Add blur radial effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -20,6 +20,7 @@ effects = [
             'blur',
             'blurx',
             'blury',
+            'blurradial',
             'oldphoto',
             'newspaperdotted',
             'judgment',
@@ -610,6 +611,27 @@ def twirl(input, output):
         output = f"{Path(input).stem}_twirl.png"
 
     instruction = "fx_twirl 1,50,50,1"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def blurradial(input, output):
+    """Applies blur radial effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_blurradial.png"
+
+    instruction = "fx_blur_radial 3,50,50,0,1,7,0"
     gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
