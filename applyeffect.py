9fb864e44a2e79eb660d1d094663ffc12ca0c64a
fernando <fernando@juandemariana.org>
2024-04-10 19:12:24 +0200

    Add twirl effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -26,6 +26,7 @@ effects = [
             'gridtriangular',
             'warhol',
             'lightpatch',
+            'twirl',
             'oilbw',
             'hearts',
             'vignette',
@@ -591,6 +592,27 @@ def waxstains(input, output):
         click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def twirl(input, output):
+    """Applies twirl effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_twirl.png"
+
+    instruction = "fx_twirl 1,50,50,1"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 @cli.command()
