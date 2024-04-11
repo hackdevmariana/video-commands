a79b4735e676928291809a3493e74f62b8c96cd5
fernando <fernando@juandemariana.org>
2024-04-11 04:14:57 +0200

    Add droste effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -692,6 +692,26 @@ def spread(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def prueba(input, output):
+    """Applies spread effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_spread.png"
+
+    instruction = "fx_droste 20,20,80,20,80,80,20,80,1,0,0,0,1,0,1,0,0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 @click.option('--intensity', type=int, default=50, help='Waves intensity')
 def stripesy(input, output, intensity):
     """Applies stripes in y axis to the received image."""
