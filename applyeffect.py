be6b5f03af970835b43efc682205512f23d2b8a9
fernando <fernando@juandemariana.org>
2024-04-11 02:05:48 +0200

    Add fxscanlines local effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -30,6 +30,7 @@ effects = [
             'judgment',
             'normalizelocal',
             'gridtriangular',
+            'fxscanlines',
             'warhol',
             'lightpatch',
             'twirl',
@@ -671,6 +672,25 @@ def wind(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+@click.option('--intensity', type=int, default=100, help='Waves intensity')
+def fxscanlines(input, output, intensity):
+    """Applies fx scanlines effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_fxscanlines.png"
+
+    instruction = f"fx_scanlines {intensity},2,0,0,0,0,0,0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def normalizelocal(input, output):
     """Applies normalize local effect to the received image."""
 
@@ -685,8 +705,6 @@ def normalizelocal(input, output):
         click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
-
-
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
