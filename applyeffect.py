2230d4238f04effbbb60080dc83e401aa899277f
fernando <fernando@juandemariana.org>
2024-04-14 04:11:45 +0200

    Add softpixelate effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -735,6 +735,27 @@ def darkmaptones(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def softpixelate(input, output):
+    """Applies soft pixelate effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_softpixelate.png"
+
+    instruction = "fx_sharpen_shock 150,0.1,0.8,1.1,1,0,0,24,0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def droste(input, output):
     """Applies droste effect to the received image."""
 
