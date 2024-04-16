62ce67fd7cd919a659c1f64c2479c42b4f2b85f0
fernando <fernando@juandemariana.org>
2024-04-16 04:03:00 +0200

    Add polka dots texture in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -27,6 +27,7 @@ effects = [
             'stripesy',
             'spread',
             'oldphoto',
+            'polkadots',
             'upscale',
             'unsharp',
             'blocks3d',
@@ -809,6 +810,26 @@ def pencilsketch(input, output):
         output = f"{Path(input).stem}_pencil_sketch.png"
 
     instruction = f'-sketchbw , -rv -blend overlay'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def polkadots(input, output):
+    """Applies polka dots texture to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_polka_dots.png"
+
+    instruction = f'fx_polka_dots 80,20,50,50,0,0.5,0.1,1,255,0,0,25'
     gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
