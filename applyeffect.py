8da255febf497e11f09fe0824667c086497b81c2
fernando <fernando@juandemariana.org>
2024-04-16 03:53:41 +0200

    Add pencil sketch effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -27,11 +27,13 @@ effects = [
             'stripesy',
             'spread',
             'oldphoto',
+            'upscale',
             'unsharp',
             'lightmaptones',
             'darkmaptones',
             'newspaperdotted',
             'deblur',
+            'pencilsketch',
             'judgment',
             'normalizelocal',
             'gridtriangular',
@@ -773,6 +775,46 @@ def unsharp(input, output):
         click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def oldtiles(input, output):
+    """Applies old tiles texture to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_old_tiles.png"
+
+    instruction = f'split_tiles 5,5 -blur 8 -sharpen 1000 -equalize 255 -append_tiles 5,5'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def pencilsketch(input, output):
+    """Applies pencil sketch effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_pencil_sketch.png"
+
+    instruction = f'-sketchbw , -rv -blend overlay'
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 
 
