629cce7710558eda0561b3033f01ae8bbf56575b
fernando <fernando@juandemariana.org>
2024-04-18 11:29:06 +0200

    Add rhombuses texture in applyefects

--- applyeffect.py
+++ applyeffect.py
@@ -1113,6 +1113,26 @@ def triangles(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def rhombuses(input, output):
+    """Applies rhombuses bw texture to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_rhombuses_bw.png"
+
+    instruction = f"fx_shapes 3,16,10,2,5,90,0,0,1,1,0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def kitt(input, output):
     """Applies Knight Rider texture to the received image."""
 
