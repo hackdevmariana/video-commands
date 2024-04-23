5c30ee0c1e90743d773a500215c4a9b8a0347ebe
fernando <fernando@juandemariana.org>
2024-04-23 23:38:43 +0200

    Add pointwise absolute value in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2194,6 +2194,27 @@ def glitteronblack(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def pointwise(input, output):
+    """Applies pointwise absolute values of  the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_pointwise_absolute.png"
+
+    instruction = "sub {ia} abs[-1]"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
