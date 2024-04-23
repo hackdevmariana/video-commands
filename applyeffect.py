7445629114c786ff0291fcc22501190a1d5fa5d0
fernando <fernando@juandemariana.org>
2024-04-23 23:50:36 +0200

    Add calor map effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2233,6 +2233,25 @@ def darkenbw(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def calormap(input, output):
+    """Applies calor map in the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_calormap.png"
+
+    instruction = "normalize -1,1 asin[-1]"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
 
 
 
