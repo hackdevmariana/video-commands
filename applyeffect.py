dd60b3ad280fa86015352c63d25326a02d334588
fernando <fernando@juandemariana.org>
2024-04-24 00:05:30 +0200

    Add sparkles effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2325,6 +2325,26 @@ def squaresoflight(input, output):
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def sparkles(input, output):
+    """Applies sparkles in the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_sparkles.png"
+
+    instruction = "div 'abs(cos(x/10)*sin(y/10))'"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
