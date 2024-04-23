8e4ec72b31843e1be9c083cb9af2eacf7a2e4772
fernando <fernando@juandemariana.org>
2024-04-23 23:55:04 +0200

    Add darken steps effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2252,6 +2252,25 @@ def calormap(input, output):
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def darkensteps(input, output):
+    """Applies darken steps in the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_darkensteps.png"
+
+    instruction = "bsl 'round(3*x/w,0)' cut 0,255"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
 
 
 
