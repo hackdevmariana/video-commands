1c6a964f0b46b810dd0f98dcf88bd19c4d8b0a9b
fernando <fernando@juandemariana.org>
2024-04-26 11:24:30 +0200

    Add black hole effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -1934,6 +1934,23 @@ def nebula(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def blackhole(input, output):
+    """Applies nebula effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_nebula.png"
+
+    instruction = "min 'R=((x/w-0.5)^2+(y/h-0.5)^2)^0.5;255*R'"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
 @cli.command()
