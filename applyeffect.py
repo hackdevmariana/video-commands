d95e14620bb8267d3c5934e160732faafec27afc
fernando <fernando@juandemariana.org>
2024-04-26 11:29:33 +0200

    Add expanding black hole effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -1972,6 +1972,25 @@ def acid(input, output):
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def expandingblackhole(input, output):
+    """Applies expanding black hole effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_expanding_black_hole.png"
+
+    instruction = "mul 'R=((x/w-0.5)^2+(y/h-0.5)^2)^0.5;255*R'"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
