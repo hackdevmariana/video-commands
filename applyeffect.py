8a523a51ee69f17ab7196c63aa2cc55439ef1f38
fernando <fernando@juandemariana.org>
2024-04-26 11:34:47 +0200

    Add light off effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2011,6 +2011,24 @@ def flash(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def lightoff(input, output):
+    """Applies light off effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_light_off.png"
+
+    instruction = "mul .3"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def badtonner(input, output):
     """Applies badtonner effect to the received image."""
 
