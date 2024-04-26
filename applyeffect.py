6c12231cd0b833b9a536bfd1b7098312590f9896
fernando <fernando@juandemariana.org>
2024-04-26 11:42:35 +0200

    Add off whites effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2029,6 +2029,24 @@ def lightoff(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def offwhites(input, output):
+    """Applies off whites effect to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_off_whites.png"
+
+    instruction = "cut 0,128"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def badtonner(input, output):
     """Applies badtonner effect to the received image."""
 
