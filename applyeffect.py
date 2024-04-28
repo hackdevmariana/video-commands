295a4842509219a7c4bf5d2b27ba70c9672b2b78
fernando <fernando@juandemariana.org>
2024-04-28 15:17:25 +0200

    Applies purple tones in image.

--- applyeffect.py
+++ applyeffect.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 from pathlib import Path
 
 import click
@@ -2054,6 +2055,24 @@ def veiled(input, output):
         output = f"{Path(input).stem}_veiled.png"
 
     instruction = "cut 128,255"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def turnspurple(input, output):
+    """Applies purple tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_purple.png"
+
+    instruction = "mul_channels 1,0.5,0.8"
     gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
