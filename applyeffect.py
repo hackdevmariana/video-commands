1c7d322b4cc2089c9f85b63162d51612e3050e0a
fernando <fernando@juandemariana.org>
2024-05-01 23:39:56 +0200

    Add black and dark green tones in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -2105,6 +2105,24 @@ def blueandblack(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def darkgreenandblack(input, output):
+    """Applies dark green and black tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_darkgreenandblack.png"
+
+    instruction = "mul_channels 0,0.2,0.1"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def americannight(input, output):
     """Applies american night tones to the received image."""
 
