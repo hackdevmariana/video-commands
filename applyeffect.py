b83d04e8c1eec6a73cf7060f87680043c6914250
fernando <fernando@juandemariana.org>
2024-05-01 23:43:46 +0200

    Add black and sea tones in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -2124,6 +2124,25 @@ def turquoiseandblack(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def seaandblack(input, output):
+    """Applies sea and black tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_seaandblack.png"
+
+    instruction = "mul_channels 0,0.2,0.3"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def darkgreenandblack(input, output):
     """Applies dark green and black tones to the received image."""
 
