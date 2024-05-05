6b54b5836c76079e08e0a87f29a8029ec320cf6d
fernando <fernando@juandemariana.org>
2024-05-05 22:54:11 +0200

    Add green and black contrasted tones in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2143,6 +2143,25 @@ def seaandblack(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def greenandblackcontrasted(input, output):
+    """Applies green and black contrasted tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_green_and_black_contrasted.png"
+
+    instruction = "mul_channels 0,0.4,0.1"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def blueandblackcontrasted(input, output):
     """Applies blue and black contrasted tones to the received image."""
 
