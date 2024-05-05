5b8c984dcaa590be9bf9c95c0224fc7dfdf53dbf
fernando <fernando@juandemariana.org>
2024-05-05 22:59:07 +0200

    Add contrasted alien tones in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2234,6 +2234,24 @@ def contrastedturquoise(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def contrastedalien(input, output):
+    """Applies contrasted alien tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_contrasted_alien.png"
+
+    instruction = "mul_channels 0,0.9,0.6"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def americannight(input, output):
     """Applies american night tones to the received image."""
 
