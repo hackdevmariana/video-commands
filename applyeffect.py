abffb53c7ca77b1d3c3aa1615483e09ecf19b0ac
fernando <fernando@juandemariana.org>
2024-05-05 23:06:48 +0200

    Add luminous red in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2234,6 +2234,24 @@ def contrastedturquoise(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def luminousred (input, output):
+    """Applies luminous red tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_luminous_red.png"
+
+    instruction = "mul_channels 1,0.1,0.4"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def redoff(input, output):
     """Applies red off tones to the received image."""
 
