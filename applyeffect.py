b39506ca9e244320aa51d253abde104c3d908785
fernando <fernando@juandemariana.org>
2024-05-07 00:07:41 +0200

    Add luminous yellow in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2339,6 +2339,25 @@ def pink (input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def luminousyellow (input, output):
+    """Applies luminous yellow tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_luminous_yellow.png"
+
+    instruction = "mul_channels 1,0.7,0.2"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
