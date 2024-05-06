73a3b0e1f6265882a46ecc2a89270f7fa8b82b4a
fernando <fernando@juandemariana.org>
2024-05-07 00:03:49 +0200

    Add intense amber in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2285,6 +2285,24 @@ def reddishorange (input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def intenseamber (input, output):
+    """Applies intense amber tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_intense_amber.png"
+
+    instruction = "mul_channels 1,0.6,0.1"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
