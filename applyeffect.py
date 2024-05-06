23e40fd67befcd5b4782e1d25e854946e4399099
fernando <fernando@juandemariana.org>
2024-05-07 00:04:51 +0200

    Add pale amber in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2303,6 +2303,24 @@ def intenseamber (input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def paleamber (input, output):
+    """Applies pale amber tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_pale_amber.png"
+
+    instruction = "mul_channels 1,0.6,0.3"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
