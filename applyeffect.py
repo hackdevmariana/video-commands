328ef1cf52d27d7f043e293939cb3dcb42211279
fernando <fernando@juandemariana.org>
2024-05-06 23:55:10 +0200

    Add dim red in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2252,13 +2252,31 @@ def luminousred (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def dimred (input, output):
+    """Applies dim red tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_dim_red.png"
+
+    instruction = "mul_channels 1,0.2,0.3"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def lilacblueluminous (input, output):
     """Applies lilac blue luminous tones to the received image."""
 
     if not output:
         output = f"{Path(input).stem}_lilac_blue_luminous.png"
 
-    instruction = "mul_channels 1,0.1,0.4"
+    instruction = "mul_channels 1,0.1,1.0"
     gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
