690c36bf03acfb2e629d48236046675bb3e9d227
fernando <fernando@juandemariana.org>
2024-05-07 00:06:04 +0200

    Add pink in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2321,6 +2321,24 @@ def paleamber (input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def pink (input, output):
+    """Applies pink tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_pink.png"
+
+    instruction = "mul_channels 1,0.6,0.8"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
