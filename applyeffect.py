993c933ea9d29aed41eb88d112c359d051d95b87
fernando <fernando@juandemariana.org>
2024-05-08 12:03:47 +0200

    Add lilac luminous tones in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -2468,6 +2468,24 @@ def orangeoff (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def lilacluminous (input, output):
+    """Applies lilac luminous tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_lilac_luminous.png"
+
+    instruction = "mul_channels 1,0.7,0.1"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def lilacblue (input, output):
     """Applies lilac and blue tones to the received image."""
 
