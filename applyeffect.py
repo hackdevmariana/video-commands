bb0c23e59ddacf4425fb0802e779cffa431ddad7
fernando <fernando@juandemariana.org>
2024-05-08 12:14:44 +0200

    Add intense yellow tones in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -2468,6 +2468,24 @@ def orangeoff (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def intenseyellow (input, output):
+    """Applies intense yellow tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_intense_yellow.png"
+
+    instruction = "mul_channels 1,0.8,0.3"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def yellow (input, output):
     """Applies yellow tones to the received image."""
 
