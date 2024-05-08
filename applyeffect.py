7178459b7b83a8fcb16e7f5b88dc7fef5b155c0d
fernando <fernando@juandemariana.org>
2024-05-08 12:20:56 +0200

    Add luminous green tones in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -2468,6 +2468,24 @@ def orangeoff (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def luminousgreen (input, output):
+    """Applies luminous green tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_luminous_green.png"
+
+    instruction = "mul_channels 1,1.0,0.1"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def lemonlime (input, output):
     """Applies lemon lime tones to the received image."""
 
