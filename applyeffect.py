5c2e07947de945bc3f21be23db86e13a2c1df2ba
fernando <fernando@juandemariana.org>
2024-05-16 00:53:15 +0200

    Add red indigo spectral tones in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2612,6 +2612,24 @@ def yellow (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def redindigospectral (input, output):
+    """Applies red indigo spectral tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_red_indigo_spectral.png"
+
+    instruction = "mul_channels -1,0.5,0.5"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def redgreenspectral (input, output):
     """Applies red green spectral tones to the received image."""
 
