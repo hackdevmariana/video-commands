17a2ef3416548eb4d72b3676d6713c9cc0d5544d
fernando <fernando@juandemariana.org>
2024-05-16 00:52:16 +0200

    Add red green spectral tones in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2612,6 +2612,24 @@ def yellow (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def redgreenspectral (input, output):
+    """Applies red green spectral tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_red_green_spectral.png"
+
+    instruction = "mul_channels -1,0.9,0.4"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def lilacluminous (input, output):
     """Applies lilac luminous tones to the received image."""
 
@@ -2644,6 +2662,8 @@ def lilacblue (input, output):
         click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
