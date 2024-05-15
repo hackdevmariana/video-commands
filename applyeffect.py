669e08430c1d847ac1b52645bb3c621241ab9be1
fernando <fernando@juandemariana.org>
2024-05-16 00:56:43 +0200

    Add red light blue spectral tones in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2612,6 +2612,24 @@ def yellow (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def redlightbluespectral (input, output):
+    """Applies red blue spectral tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_red_light_blue_spectral.png"
+
+    instruction = "mul_channels -1,0.6,0.9"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def redbluespectral (input, output):
     """Applies red blue spectral tones to the received image."""
 
