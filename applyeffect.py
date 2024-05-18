744d1a16152b7b5dbe81700377a4f90918d574ca
fernando <fernando@juandemariana.org>
2024-05-19 00:00:44 +0200

    Add pale indigo effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2772,7 +2772,23 @@ def paleblue (input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def paleindigo (input, output):
+    """Applies pale indigo tones to the received image."""
 
+    if not output:
+        output = f"{Path(input).stem}_pale_indigo.png"
+
+    instruction = "mul_channels 0.7,0.9,0.5"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
@@ -2791,8 +2807,6 @@ def intensedarklime (input, output):
         click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
-
-
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
