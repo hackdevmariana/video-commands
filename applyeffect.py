c33677faab9c82344679837e41be8c2d9aaa2ec7
fernando <fernando@juandemariana.org>
2024-05-19 00:14:13 +0200

    Add dark intense purple effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2775,6 +2775,24 @@ def paleblue (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def darkintensepurple (input, output):
+    """Applies dark intense purple tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_dark_intense_purple.png"
+
+    instruction = "mul_channels 0.8,0.0,0.6"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def paleindigo (input, output):
     """Applies pale indigo tones to the received image."""
 
