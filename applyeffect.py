0fae9d124b62d1c389fcbe04b4de72c9f18c2a72
fernando <fernando@juandemariana.org>
2024-05-18 23:58:28 +0200

    Add natural dark yellow effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2754,6 +2754,24 @@ def luminousindigo (input, output):
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def naturaldarkyellow (input, output):
+    """Applies natural dark yellow tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_natural_dark_yellow.png"
+
+    instruction = "mul_channels 0.7,0.6,0.1"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
