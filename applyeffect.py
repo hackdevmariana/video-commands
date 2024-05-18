0414640482b610a245f67e5d9b9f5d38102fa3b6
fernando <fernando@juandemariana.org>
2024-05-19 00:27:04 +0200

    Add intense lime effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2775,6 +2775,24 @@ def paleblue (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def intenselime (input, output):
+    """Applies intense lime tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_intense_lime.png"
+
+    instruction = "mul_channels 0.8,0.9,0.1"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def darkintensepurple (input, output):
     """Applies dark intense purple tones to the received image."""
 
