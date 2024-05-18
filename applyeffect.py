9760a7d81080d151291cfb4c21bf472175f1dbf7
fernando <fernando@juandemariana.org>
2024-05-18 23:56:44 +0200

    Add dark yellow effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2720,6 +2720,25 @@ def redgreenspectral (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def darkyellow (input, output):
+    """Applies dark yellow tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_dark_yellow.png"
+
+    instruction = "mul_channels 0.7,0.5,0.1"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def darkorange (input, output):
     """Applies dark orange tones to the received image."""
 
