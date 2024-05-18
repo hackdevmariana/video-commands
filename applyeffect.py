96b798b4b36bb483155fa3f61980f48050674437
fernando <fernando@juandemariana.org>
2024-05-18 23:55:54 +0200

    Add dark orange effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2716,6 +2716,25 @@ def redgreenspectral (input, output):
         click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def darkorange (input, output):
+    """Applies dark orange tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_dark_orange.png"
+
+    instruction = "mul_channels 0.7,0.4,0.0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
