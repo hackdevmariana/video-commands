4065dbd3ffc2064747d06e46cd339c2b809583ff
fernando <fernando@juandemariana.org>
2024-05-18 23:54:59 +0200

    Add dark purple effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2716,6 +2716,23 @@ def redgreenspectral (input, output):
         click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def darkpurple (input, output):
+    """Applies dark purple tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_dark_purple.png"
+
+    instruction = "mul_channels 0.7,0.2,0.9"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
@@ -2906,7 +2923,7 @@ def americannight(input, output):
 def prueba(input, output):
     """Applies purple tones to the received image."""
 
-    num_1 = -1
+    num_1 = round(uniform(0,1), 1)
     num_2 = round(uniform(0,1), 1)
     num_3 = round(uniform(0,1), 1)
 
