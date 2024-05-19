56e9117c02d18d1912c213e062102bed6b82c4eb
fernando <fernando@juandemariana.org>
2024-05-19 09:33:52 +0200

    Add dark blue black effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2811,6 +2811,24 @@ def intenseyellow (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def darkblueblack (input, output):
+    """Applies dark blue black tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_dark_blue_black.png"
+
+    instruction = "mul_channels 0.0,0.0,0.7"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def orangeblack (input, output):
     """Applies orange black tones to the received image."""
 
