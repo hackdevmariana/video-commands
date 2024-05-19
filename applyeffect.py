6aa705a3ce7733cfbd42d27ce142d3306dc5f849
fernando <fernando@juandemariana.org>
2024-05-19 09:25:28 +0200

    Add luminous red black effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2775,6 +2775,24 @@ def paleblue (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def luminousredblack (input, output):
+    """Applies luminous red black tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_luminous_red_black.png"
+
+    instruction = "mul_channels 0.9,0.0,0.0"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def intenselime (input, output):
     """Applies intense lime tones to the received image."""
 
