d8406318d8036fa31925b5f1c2d4e2709ddda928
fernando <fernando@juandemariana.org>
2024-05-20 15:19:15 +0200

    Add dark night tone in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 from pathlib import Path
 
 import click
@@ -2854,6 +2853,24 @@ def darkintenselilac (input, output):
         output = f"{Path(input).stem}_dark_intense_lilac.png"
 
     instruction = "mul_channels 1.0,0.0,0.9"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def darknight (input, output):
+    """Applies dark night tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_dark_night.png"
+
+    instruction = "mul_channels 0.0,0.1,0.3"
     gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
