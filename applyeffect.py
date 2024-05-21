7f29f0d53c09e7d66688855025d422db3ad07a81
fernando <fernando@juandemariana.org>
2024-05-21 11:05:00 +0200

    Add yellow and sky tone in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -3353,6 +3353,24 @@ def maritime(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def yellowandsky(input, output):
+    """Applies yellow and sky tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_yellow_and_sky.png"
+
+    instruction = "mul_channels -0.8,-0.4,-0.7"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def whiteandsky(input, output):
     """Applies white and sky tones to the received image."""
 
