cd34d20c63ea456842bceaf4b8520485380d824a
fernando <fernando@juandemariana.org>
2024-05-21 10:05:40 +0200

    Add maritime tone in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -3332,6 +3332,24 @@ def electricgreen(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def maritime(input, output):
+    """Applies maritime tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_maritime.png"
+
+    instruction = "mul_channels 0,-0.5,-0.9"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
