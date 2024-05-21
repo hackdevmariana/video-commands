7e71890eb85a1f0aa3352368ed1bff763cfb76bc
fernando <fernando@juandemariana.org>
2024-05-21 10:11:24 +0200

    Add pine tree tone in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -3353,6 +3353,24 @@ def maritime(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def pinetree(input, output):
+    """Applies pine tree tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_pine_tree.png"
+
+    instruction = "mul_channels 0,0.8,0.4"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def palesky(input, output):
     """Applies pale sky tones to the received image."""
 
