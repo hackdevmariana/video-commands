c34b3e44ab2153ca5ad1e7cb87c417b44c2724c2
fernando <fernando@juandemariana.org>
2024-05-22 13:30:30 +0200

    Add purple and white tone in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -3353,6 +3353,24 @@ def maritime(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def purplewhite(input, output):
+    """Applies purple and white tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_purple_and_white.png"
+
+    instruction = "mul_channels -0.8,-1.0,-0.3"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def purpleyellow(input, output):
     """Applies purple and yellow tones to the received image."""
 
