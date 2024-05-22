630a5a90dd7bda37879e91384a348b4300829c13
fernando <fernando@juandemariana.org>
2024-05-22 23:08:08 +0200

    Add wine and white tone in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -3353,6 +3353,24 @@ def maritime(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def wineandwhite(input, output):
+    """Applies wine and white tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_wine_and_white.png"
+
+    instruction = "mul_channels -0.9,-1.0,-0.5"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def tealandwhite(input, output):
     """Applies teal and white tones to the received image."""
 
