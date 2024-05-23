344f96dac8343a51b16ee34fdb12a522da9eff5c
fernando <fernando@juandemariana.org>
2024-05-23 18:24:26 +0200

    Add contrasted blue tone in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -3371,6 +3371,24 @@ def electricalienandwhite(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def contrastedblue(input, output):
+    """Applies contrasted blue tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_contrasted_blue.png"
+
+    instruction = "mul_channels 0.1,-1.0,-0.3"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def spectralblue(input, output):
     """Applies spectral blue tones to the received image."""
 
