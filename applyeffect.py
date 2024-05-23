2d720160ef78d7a7733bdc6cac22a7d66642e949
fernando <fernando@juandemariana.org>
2024-05-23 15:09:37 +0200

    Add alien cobalt tone in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -3371,6 +3371,24 @@ def electricalienandwhite(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def aliencobalt(input, output):
+    """Applies alien cobalt tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_alien_cobalt.png"
+
+    instruction = "mul_channels 0.0,-0.5,0.4"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def intensepinkandblack(input, output):
     """Applies intense pink and black tones to the received image."""
 
