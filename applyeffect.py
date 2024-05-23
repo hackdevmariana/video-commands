c08c196298c166e68211144febb4b7f06e3dc7bd
fernando <fernando@juandemariana.org>
2024-05-23 18:18:34 +0200

    Add spectral sky tone in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -3371,6 +3371,24 @@ def electricalienandwhite(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def spectralsky(input, output):
+    """Applies spectral sky tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_spectral_sky.png"
+
+    instruction = "mul_channels 0.1,-0.4,-0.4"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def seaweed(input, output):
     """Applies seaweed tones to the received image."""
 
