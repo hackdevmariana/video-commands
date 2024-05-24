e9fa05637086cc1c457cb9434ff25b8221b88fe9
fernando <fernando@juandemariana.org>
2024-05-24 11:58:28 +0200

    Add dark and outline effect in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -4058,10 +4058,29 @@ def electricgreenblue(input, output):
 def prueba(input, output):
     """Applies purple tones to the received image."""
 
-    instruction = "div 255 pow 0.5 mul 255"
+    instruction = "gradient pow 8 add pow 0.1"
 
     if not output:
         output = f"{Path(input).stem}_neq.png"
+
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def darkandoutline(input, output):
+    """Applies dark and outline effect to the received image."""
+
+    instruction = "gradient pow 8 add pow 0.1"
+
+    if not output:
+        output = f"{Path(input).stem}_dark_and_outline.png"
 
     gmic.run(f'{input} {instruction} output {output}')
 
