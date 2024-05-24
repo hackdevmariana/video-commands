47667a6c6a4ff3f007971f7cf988c63431f74d69
fernando <fernando@juandemariana.org>
2024-05-24 10:54:54 +0200

    Add subtle blue tone in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -2846,6 +2846,24 @@ def orangeblack (input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def subtleblue (input, output):
+    """Applies subtle blue tones to the received image."""
+
+    if not output:
+        output = f"{Path(input).stem}_subtle_blue.png"
+
+    instruction = "mul_channels 0.2,-0.6,-0.1"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def darkintenselilac (input, output):
     """Applies dark intense lilac tones to the received image."""
 
