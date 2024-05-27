747eb30a20cb738c3955dcfadd273fdbaeb28198
fernando <fernando@juandemariana.org>
2024-05-27 02:54:12 +0200

    Create changecolor command

---
+++ changecolor.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+import click
+import colorama
+import gmic
+
+
+
+def process_image(command_name, instruction):
+    def decorator(func):
+        @click.command(name=command_name)
+        @click.argument('input', type=click.Path(exists=True))
+        @click.option('--output', '-o', default='', help='Output file path')
+        def wrapper(input, output):
+            if not output:
+                output = f"{Path(input).stem}_{command_name}.png"
+
+            gmic.run(f'{input} {instruction} output {output}')
+
+            output_path = Path(output)
+            if output_path.is_file():
+                click.echo(f"The image has been created{colorama.Fore.GREEN} successfully{colorama.Style.RESET_ALL}: {output}")
+            else:
+                click.echo(f"An{colorama.Fore.RED} error{colorama.Style.RESET_ALL} occurred creating the file {output}.")
+
+        return wrapper
+    return decorator
+
+@process_image('orange', "balance_gamma 241,147,-121")
+def orange():
+    """Changes the color of the received image to orange."""
+
+@click.group()
+def cli():
+    pass
+
+cli.add_command(orange)
+
+if __name__ == "__main__":
+    cli()
