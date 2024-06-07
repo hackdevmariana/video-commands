d444398eb59809f738571834adc59d523289cbf0
fernando <fernando@juandemariana.org>
2024-06-08 00:18:43 +0200

    Add lucy in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -4118,12 +4118,12 @@ def spectral(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
-def dottedbw(input, output):
-    """Applies dotted black and white to the received image."""
+def lucy(input, output):
+    """Applies lysergic tones to the received image."""
 
-    instruction = f"rgb2bayer 1"
+    instruction = f"luminance gradient append c blur 2 orientation direction2rgb"
 
-    output = f"{Path(input).stem}_dottedbw.png"
+    output = f"{Path(input).stem}_deltaE.png"
     gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
@@ -4137,6 +4137,23 @@ def dottedbw(input, output):
 @click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies purple tones to the received image."""
+
+    instruction = f"luminance gradient append c blur 2 orientation direction2rgb"
+
+    output = f"{Path(input).stem}_deltaE.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def dottedbw(input, output):
+    """Applies dotted black and white to the received image."""
 
     instruction = f"rgb2bayer 1"
 
