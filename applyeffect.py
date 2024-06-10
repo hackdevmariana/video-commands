67df3874ee9053cac1a87d9f484478c222d6dc81
fernando <fernando@juandemariana.org>
2024-06-10 12:52:27 +0200

    Add coloredtelesketch in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4363,10 +4363,27 @@ def erodecirc(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def multipoint(input, output):
+    """Applies multiple stippling to the received image."""
+
+    instruction = 'iee'
+    if not output:
+        output = f"{Path(input).stem}_solarize.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies purple tones to the received image."""
 
-    instruction = 'iee'
+    instruction = 'inn'
     if not output:
         output = f"{Path(input).stem}_solarize.png"
     gmic.run(f'{input} {instruction} output {output}')
