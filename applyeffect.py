db3930db953c22738a0d4824e4e025387cc35057
fernando <fernando@juandemariana.org>
2024-06-13 00:11:45 +0200

    Add shifttiles in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4753,10 +4753,27 @@ def rotatetiles(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def shifttiles(input, output):
+    """Applies shift tiles to the received image."""
+
+    instruction = 'shift_tiles 8,8,10'
+    if not output:
+        output = f"{Path(input).stem}_shift_tiles.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies frame fuzzy to the received image."""
 
-    instruction = 'to_rgba rotate_tiles 10,8 drop_shadow 10,10 display_rgba'
+    instruction = 'shift_tiles 8,8,10'
     if not output:
         output = f"{Path(input).stem}_quada.png"
     gmic.run(f'{input} {instruction} output {output}')
