706ade544d4e401e9108ba90c612ea81b12862d5
fernando <fernando@juandemariana.org>
2024-06-12 23:55:45 +0200

    Add vignetteblur in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4736,6 +4736,40 @@ def vignetteblur(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def prueba(input, output):
+    """Applies vignette blur to the received image."""
+
+    instruction = 'frame_cube ,'
+    if not output:
+        output = f"{Path(input).stem}_vignetteblur.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def framecube(input, output):
+    """Applies frame cube effect to the received image."""
+
+    instruction = 'frame_cube ,'
+    if not output:
+        output = f"{Path(input).stem}_frame_cube.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def dottedbw(input, output):
     """Applies dotted black and white to the received image."""
 
