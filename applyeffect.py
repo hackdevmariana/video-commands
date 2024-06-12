a1773d1d81d7243ce0fdcae81e5977e76430484b
fernando <fernando@juandemariana.org>
2024-06-12 15:29:30 +0200

    Add arraymirror in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4702,10 +4702,27 @@ def fourfour(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def arraymirror(input, output):
+    """Applies array mirror effect to the received image."""
+
+    instruction = 'array_mirror 1'
+    if not output:
+        output = f"{Path(input).stem}_arraymirror.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies dotted black and white to the received image."""
 
-    instruction = 'array_fade 4,4'
+    instruction = 'array_mirror 1'
     if not output:
         output = f"{Path(input).stem}_tones.png"
     gmic.run(f'{input} {instruction} output {output}')
