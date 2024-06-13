35a6baca75d07b11d22d23cb692325bd59d7bb95
fernando <fernando@juandemariana.org>
2024-06-14 00:01:53 +0200

    Add coloredmarker in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4924,6 +4924,23 @@ def colorareas(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def coloredmarker(input, output):
+    """Applies colored with marker texture to the received image."""
+
+    instruction = 'cartoon 5,144,111,160.23,8.71,42'
+    if not output:
+        output = f"{Path(input).stem}_coloredmarker.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def intenseposter(input, output):
     """Applies intense poster texture to the received image."""
 
