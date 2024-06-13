8155fffd7bb86c30a5b64e7ffc6acf193a470634
fernando <fernando@juandemariana.org>
2024-06-14 00:25:35 +0200

    Add pressing in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4992,6 +4992,23 @@ def brightdetails(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def pressing(input, output):
+    """Applies pressing texture to the received image."""
+
+    instruction = 'cartoon 9,146,41,37.4,0.34,134'
+    if not output:
+        output = f"{Path(input).stem}_pressing.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def flatcomic(input, output):
     """Applies flat comic texture to the received image."""
 
