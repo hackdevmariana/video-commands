3749694f5b9c9a9fa2569dd1a82f5fb149c641e7
fernando <fernando@juandemariana.org>
2024-06-13 23:55:03 +0200

    Add colorareas in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4904,6 +4904,22 @@ def greatpointillism(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def colorareas(input, output):
+    """Applies color areas texture to the received image."""
+
+    instruction = 'cartoon 5,56,79,126.82,8.58,163'
+    if not output:
+        output = f"{Path(input).stem}_colorareas.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
