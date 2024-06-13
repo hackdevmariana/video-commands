c127653180915b8b89914325e124fc8d1ba3d5a1
fernando <fernando@juandemariana.org>
2024-06-14 00:10:19 +0200

    Add flattenscolorsbw in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4924,6 +4924,23 @@ def flattenscolors(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def flattenscolorsbw(input, output):
+    """Applies flattens colors in black and white texture to the received image."""
+
+    instruction = 'cartoon 6,131,20,157.73,0.08,167'
+    if not output:
+        output = f"{Path(input).stem}_flattenscolorsbw.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def uniformstains(input, output):
     """Applies uniform stains texture to the received image."""
 
