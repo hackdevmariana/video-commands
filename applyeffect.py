b9b12d618b3361ed180c8295830dfa924dee1d54
fernando <fernando@juandemariana.org>
2024-06-14 00:16:44 +0200

    Add handstrokes in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4975,6 +4975,23 @@ def aqueousink(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def handstrokes(input, output):
+    """Applies hand strokes texture to the received image."""
+
+    instruction = 'cartoon 8,81,26,125.74,17.21,199'
+    if not output:
+        output = f"{Path(input).stem}_handstrokes.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def uniformstains(input, output):
     """Applies uniform stains texture to the received image."""
 
