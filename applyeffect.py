aaecce1432494be494350099cdb3cc4a4fb93573
fernando <fernando@juandemariana.org>
2024-06-14 00:27:22 +0200

    Add colorsketch in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -5009,6 +5009,23 @@ def pressing(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def colorsketch(input, output):
+    """Applies color sketch texture to the received image."""
+
+    instruction = 'cartoon 10,90,171,56.34,3.81,37'
+    if not output:
+        output = f"{Path(input).stem}_colorsketch.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def flatcomic(input, output):
     """Applies flat comic texture to the received image."""
 
