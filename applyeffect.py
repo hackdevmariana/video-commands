17a6bc31a1cb1e7aa0b92688ead49e14fefb3097
fernando <fernando@juandemariana.org>
2024-06-13 22:23:46 +0200

    Add greatpointillism in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4886,6 +4886,25 @@ def colorfulmural(input, output):
     else:
         click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
 
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def greatpointillism(input, output):
+    """Applies great pointillism texture to the received image."""
+
+    instruction = 'cartoon 5,56,79,126.82,8.58,163'
+    if not output:
+        output = f"{Path(input).stem}_greatpointillism.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
