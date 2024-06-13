15d6af05f602c1fe161f284fe3e545dcda44c52b
fernando <fernando@juandemariana.org>
2024-06-14 00:18:45 +0200

    Add brightdetails in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4975,6 +4975,23 @@ def aqueousink(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def brightdetails(input, output):
+    """Applies bright details texture to the received image."""
+
+    instruction = 'cartoon 8,145,55,165.72,23.25,195'
+    if not output:
+        output = f"{Path(input).stem}_brightdetails.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def handstrokes(input, output):
     """Applies hand strokes texture to the received image."""
 
