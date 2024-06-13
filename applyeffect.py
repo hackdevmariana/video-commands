ee830ca54d5e1fee036f6f7f6d89e82cae4c045a
fernando <fernando@juandemariana.org>
2024-06-14 00:20:53 +0200

    Add hero in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4992,6 +4992,23 @@ def brightdetails(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def hero(input, output):
+    """Applies hero texture to the received image."""
+
+    instruction = 'cartoon 8,148,188,56.23,10.25,183'
+    if not output:
+        output = f"{Path(input).stem}_hero.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def handstrokes(input, output):
     """Applies hand strokes texture to the received image."""
 
