a094fd0a7e6add53e2034cdddac228c16ec40427
fernando <fernando@juandemariana.org>
2024-06-14 00:06:56 +0200

    Add uniformstains in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4907,6 +4907,23 @@ def greatpointillism(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def uniformstains(input, output):
+    """Applies uniform stains texture to the received image."""
+
+    instruction = 'cartoon 6,58,109,139.49,19.61,180 blur 1'
+    if not output:
+        output = f"{Path(input).stem}_uniformstains.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def colorareas(input, output):
     """Applies color areas texture to the received image."""
 
