65f4175a1ee652ad4d8e6a25d95c022af2ea1bba
fernando <fernando@juandemariana.org>
2024-06-13 15:10:02 +0200

    Add oilstains in applyeffects

--- applyeffect.py
+++ applyeffect.py
@@ -4804,8 +4804,25 @@ def vinylstains(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def oilstains(input, output):
+    """Applies oil stains texture to the received image."""
+
+    instruction = 'cartoon 4,130,142,54.86,17.18,119'
+    if not output:
+        output = f"{Path(input).stem}_oilstains.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def fingerpaint(input, output):
-    """Applies intense stains texture to the received image."""
+    """Applies finger paint texture to the received image."""
 
     instruction = 'cartoon 11,117,26,35.23,21.99,69'
     if not output:
