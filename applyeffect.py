c300e5477d7816c2af637d2ef220176e0c6cda73
fernando <fernando@juandemariana.org>
2024-06-16 00:45:25 +0200

    Add drawing in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5213,6 +5213,23 @@ def cubism(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def drawing(input, output):
+    """Applies drawing texture to the received image."""
+
+    instruction = 'drawing ,'
+    if not output:
+        output = f"{Path(input).stem}_whirl.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies frame fuzzy to the received image."""
 
