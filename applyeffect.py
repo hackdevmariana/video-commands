431e0846a6380b31e5d50d0390df2cd9e6fbe50f
fernando <fernando@juandemariana.org>
2024-06-16 00:46:36 +0200

    Add drawing in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5218,7 +5218,24 @@ def drawing(input, output):
 
     instruction = 'drawing ,'
     if not output:
-        output = f"{Path(input).stem}_whirl.png"
+        output = f"{Path(input).stem}_drawing.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def ellipsionism(input, output):
+    """Applies ellipsionism texture to the received image."""
+
+    instruction = 'ellipsionism ,'
+    if not output:
+        output = f"{Path(input).stem}_ellipsionism.png"
     gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
