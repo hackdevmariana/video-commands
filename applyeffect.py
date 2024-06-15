a8afff5b06b7d3c54cc07b525f6d29a0bd35a4e1
fernando <fernando@juandemariana.org>
2024-06-16 01:19:07 +0200

    Add fractalize in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5247,6 +5247,23 @@ def fireedges(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def fractalize(input, output):
+    """Applies fire edges texture to the received image."""
+
+    instruction = 'fractalize ,'
+    if not output:
+        output = f"{Path(input).stem}_fractalize.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def ellipsionism(input, output):
     """Applies ellipsionism texture to the received image."""
 
