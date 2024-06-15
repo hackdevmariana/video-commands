a81845d5614fbcaefe5aa9f373f868ec8b6bc499
fernando <fernando@juandemariana.org>
2024-06-16 01:33:04 +0200

    Add handsketch in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5315,6 +5315,23 @@ def hardsketchbw(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def handsketch(input, output):
+    """Applies blurry sketch texture to the received image."""
+
+    instruction = 'hardsketchbw 100,50,0.1,10'
+    if not output:
+        output = f"{Path(input).stem}_handsketch.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies frame fuzzy to the received image."""
 
