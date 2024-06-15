fd874a0dc1203bdb8e5c14a020b10ffa094d86a4
fernando <fernando@juandemariana.org>
2024-06-16 01:30:59 +0200

    Add hardsketchbw in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5298,6 +5298,23 @@ def blurrysketch(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def hardsketchbw(input, output):
+    """Applies blurry sketch texture to the received image."""
+
+    instruction = 'hardsketchbw 200,70,0.1,10 median 2 reverse'
+    if not output:
+        output = f"{Path(input).stem}_hardsketchbw.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies frame fuzzy to the received image."""
 
