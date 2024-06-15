ec63d7bd69161ef068d487f9f2360be969073320
fernando <fernando@juandemariana.org>
2024-06-16 00:00:23 +0200

    Add colorellipses in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5179,6 +5179,23 @@ def airbrushing(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def colorellipses(input, output):
+    """Applies color ellipses texture to the received image."""
+
+    instruction = 'color_ellipses ,,0.15'
+    if not output:
+        output = f"{Path(input).stem}_color_ellipses.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies frame fuzzy to the received image."""
 
