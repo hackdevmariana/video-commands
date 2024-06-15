055ce8050c25ec36af832d643890d82a55742765
fernando <fernando@juandemariana.org>
2024-06-16 00:01:10 +0200

    Add cubism in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5196,6 +5196,23 @@ def colorellipses(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def cubism(input, output):
+    """Applies cubism texture to the received image."""
+
+    instruction = 'cubism ,'
+    if not output:
+        output = f"{Path(input).stem}_cubism.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies frame fuzzy to the received image."""
 
