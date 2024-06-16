4d3326d176eda3482d72f3902ff02f64a12a4cb6
fernando <fernando@juandemariana.org>
2024-06-16 02:07:46 +0200

    Add pencilbw in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5351,6 +5351,23 @@ def linify(input, output, intensity):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def pencilbw(input, output):
+    """Applies pencil black and white texture to the received image."""
+
+    instruction = 'pencilbw ,'
+    if not output:
+        output = f"{Path(input).stem}_pencilbw.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies frame fuzzy to the received image."""
 
@@ -5363,10 +5380,10 @@ def prueba(input, output):
     # instruction = f'cartoon {value_1},{value_2},{value_3},{value_4},{value_5},{value_6}'
 
 
-    instruction = f'blur 2 light_relief 0.3,0.1,0.1,0'
+    instruction = f'pencilbw ,'
     if not output:
         # output = f"{Path(input).stem}_{instruction.replace(' ', '_').replace(',', '_')}.png"
-        output = f"{Path(input).stem}_.png"
+        output = f"{Path(input).stem}_pencilbw.png"
     gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
