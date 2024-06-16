b0f4cee20aba6812b32f475aeb8ba9908ec840a7
fernando <fernando@juandemariana.org>
2024-06-16 02:12:33 +0200

    Add posteredges in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5368,6 +5368,23 @@ def pencilbw(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def posteredges(input, output):
+    """Applies poster edges texture to the received image."""
+
+    instruction = 'poster_edges ,'
+    if not output:
+        output = f"{Path(input).stem}_poster_edges.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies frame fuzzy to the received image."""
 
@@ -5380,7 +5397,7 @@ def prueba(input, output):
     # instruction = f'cartoon {value_1},{value_2},{value_3},{value_4},{value_5},{value_6}'
 
 
-    instruction = f'pencilbw ,'
+    instruction = f'poster_edges ,'
     if not output:
         # output = f"{Path(input).stem}_{instruction.replace(' ', '_').replace(',', '_')}.png"
         output = f"{Path(input).stem}_pencilbw.png"
