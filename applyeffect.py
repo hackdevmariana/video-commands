03ec987b12a7241a1dafc154f2bc4116f63473cb
fernando <fernando@juandemariana.org>
2024-06-17 12:45:38 +0200

    Add areasbw in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5424,7 +5424,24 @@ def stainsbw(input, output):
 
     instruction = 'stencilbw 7,0'
     if not output:
-        output = f"{Path(input).stem}_subtle_drawing.png"
+        output = f"{Path(input).stem}_stainsbw.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def areasbw(input, output):
+    """Applies areas black and white to the received image."""
+
+    instruction = 'stencilbw 18,24'
+    if not output:
+        output = f"{Path(input).stem}_areasbw.png"
     gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
