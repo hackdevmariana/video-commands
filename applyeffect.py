7d5cb890d37eaab6786336228fe08b9d73789d6a
fernando <fernando@juandemariana.org>
2024-06-17 12:47:01 +0200

    Add inked in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5453,6 +5453,23 @@ def areasbw(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def inked(input, output):
+    """Applies inked black and white texture to the received image."""
+
+    instruction = 'stencilbw 25,3'
+    if not output:
+        output = f"{Path(input).stem}_inked.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies frame fuzzy to the received image."""
 
