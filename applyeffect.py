d100bbe0d54dfc12f814a78ee75cf9c548ab05d6
fernando <fernando@juandemariana.org>
2024-06-18 14:51:07 +0200

    Add recreation in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5657,6 +5657,23 @@ def implosion(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def recreation(input, output):
+    """Applies recreation effect to the received image."""
+
+    instruction = 'transform_polar "R-r",a'
+    if not output:
+        output = f"{Path(input).stem}_recreation.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies frame fuzzy to the received image."""
 
@@ -5669,7 +5686,7 @@ def prueba(input, output):
     # instruction = f'cartoon {value_1},{value_2},{value_3},{value_4},{value_5},{value_6}'
 
 
-    instruction = f'transform_polar R-(r/R)^2,a'
+    instruction = f'transform_polar "R-r",a'
     if not output:
         # output = f"{Path(input).stem}_{instruction.replace(' ', '_').replace(',', '_')}.png"
         output = f"{Path(input).stem}_rodilius.png"
