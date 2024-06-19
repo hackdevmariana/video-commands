87d26f941b01c225a53b9599fe729f570e878217
fernando <fernando@juandemariana.org>
2024-06-19 09:34:22 +0200

    Add pixelize in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5708,6 +5708,23 @@ def breeze(input, output):
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
+def pixelize(input, output):
+    """Applies pixelize effect to the received image."""
+
+    instruction = 'pixelize ,'
+    if not output:
+        output = f"{Path(input).stem}_pixelize.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
 def prueba(input, output):
     """Applies frame fuzzy to the received image."""
 
@@ -5720,7 +5737,7 @@ def prueba(input, output):
     # instruction = f'cartoon {value_1},{value_2},{value_3},{value_4},{value_5},{value_6}'
 
 
-    instruction = 'wind ,'
+    instruction = 'pixelize ,'
     if not output:
         # output = f"{Path(input).stem}_{instruction.replace(' ', '_').replace(',', '_')}.png"
         output = f"{Path(input).stem}_rodilius.png"
