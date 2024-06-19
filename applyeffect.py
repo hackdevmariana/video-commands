fe6d6803aa89f19ca6ce4de507da8ad2b6dcab95
fernando <fernando@juandemariana.org>
2024-06-19 09:32:06 +0200

    Add breeze in applyeffect

--- applyeffect.py
+++ applyeffect.py
@@ -5675,11 +5675,28 @@ def recreation(input, output):
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--output', '-o', default='', help='Output file path')
 def warpperspective(input, output):
-    """Applies warp_perspective effect to the received image."""
+    """Applies warp perspective effect to the received image."""
 
     instruction = 'warp_perspective ,'
     if not output:
         output = f"{Path(input).stem}_warp_perspective.png"
+    gmic.run(f'{input} {instruction} output {output}')
+
+    output_path = Path(output)
+    if output_path.is_file():
+        click.echo(f"The image has been created{ colorama.Fore.GREEN } successfully{ colorama.Style.RESET_ALL }: {output}")
+    else:
+        click.echo(f"An{ colorama.Fore.RED } error{ colorama.Style.RESET_ALL } occurred creating the file {output}.")
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def breeze(input, output):
+    """Applies breeze effect to the received image."""
+
+    instruction = 'wind ,'
+    if not output:
+        output = f"{Path(input).stem}_breeze.png"
     gmic.run(f'{input} {instruction} output {output}')
 
     output_path = Path(output)
@@ -5703,7 +5720,7 @@ def prueba(input, output):
     # instruction = f'cartoon {value_1},{value_2},{value_3},{value_4},{value_5},{value_6}'
 
 
-    instruction = f'warp_perspective ,'
+    instruction = 'wind ,'
     if not output:
         # output = f"{Path(input).stem}_{instruction.replace(' ', '_').replace(',', '_')}.png"
         output = f"{Path(input).stem}_rodilius.png"
