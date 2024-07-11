e8079fcab8ee4acfcc8905bc3bf138e48a165efe
fernando <fernando@juandemariana.org>
2024-07-12 00:07:21 +0200

    Add the composite merge inside the combine_images function of the merge_images.py command.

--- merge_images.py
+++ merge_images.py
@@ -107,6 +107,11 @@ def combine_images(image_1, image_2):
         output = f"{Path(image_1).stem}_{Path(image_2).stem}_blend.png"
         add_image.save(output)
 
+        # ImageChops.composite
+        add_image = ImageChops.composite(img_1, img_2)
+        output = f"{Path(image_1).stem}_{Path(image_2).stem}_composite.png"
+        add_image.save(output)
+
 if __name__ == '__main__':
     image_1 = 'image.jpeg'
     image_2 = 'image.png'
