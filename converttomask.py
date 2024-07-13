c905346106e2959191410a0c034793bb5138c7a3
fernando <fernando@juandemariana.org>
2024-07-13 21:59:11 +0200

    Add the inked subcommand to the converttomask.py command.

--- converttomask.py
+++ converttomask.py
@@ -44,6 +44,14 @@ def whirl(input, output):
 def eddy(input, output):
     """Transforms the received image into a mask with an eddy texture."""
     apply_filter(input, output, 'draw_whirl 232 luminance', 'mask_eddy')
+
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def inked(input, output):
+    """Transforms the received image into a mask with an inked texture."""
+    apply_filter(input, output, 'stencilbw 70', 'mask_inked')
+
     
 if __name__ == '__main__':
     colorama.init()  # Initialize colorama
