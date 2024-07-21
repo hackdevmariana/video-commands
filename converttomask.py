e681215cf0c3d2f10127720e9481ae44b8b5bd5f
hackdevmariana <159154214+hackdevmariana@users.noreply.github.com>
2024-07-22 00:35:02 +0200

    Add the whiteboard subcommand to the converttomask.py command.

--- converttomask.py
+++ converttomask.py
@@ -437,6 +437,13 @@ def ironmaiden(input, output):
     """Transforms the received image into a mask with an iron maiden texture."""
     apply_filter(input, output, 'apply_gamma 0.6,1.4,0.8 structuretensors xor luminance', 'mask_ironmaiden')
 
+@cli.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--output', '-o', default='', help='Output file path')
+def whiteboard(input, output):
+    """Transforms the received image into a mask with a whiteboard texture."""
+    apply_filter(input, output, 'fx_pencil_portraitbw 33,40,73,77,44,136 structuretensors mod luminance', 'mask_whiteboard')
+    
 if __name__ == '__main__':
     colorama.init()  # Initialize colorama
     cli()
